f1654804a076aea0affb68654de3d4691cad55d6
Welerson <luizpawlak7@gmail.com>
2019-05-13 09:06:29 -0300

    Idk, a bunch of little alterations.

--- js/comics_slide.js
+++ js/comics_slide.js
@@ -1,16 +1,31 @@
-var slider = document.getElementById("comics-container");
+var slider = document.getElementById("comics-list");
 
-temp = slider.style.left;
+position_slider = slider.style.left;
+
 function buttonNext(){
-    console.log("Clique");
-
-    console.log(temp);
-    if (slider.style.left == temp){
+    console.log(position_slider);
+    
+    if (slider.style.left == position_slider){
         slider.style.left = (-280*4) + "px";
-        console.log("if");
     }else{
-        slider.style.left = 0 + "px";
-        console.log("else");
+        slider.style.left = (-100) + "px";
+        setTimeout(function slideBlock(){
+                slider.style.left = 0 + "px";
+                console.log(slider.style.left);
+            }, 100);
     }
-    console.log(slider.style.left);
+}
+
+
+function buttonPrevious(){
+
+    if (slider.style.left == "0px"){
+        slider.style.left = (-300) + "px";
+        setTimeout(function slideBlock(){
+                slider.style.left = 0 + "px";
+                console.log(slider.style.left);
+            }, 50);
+    }else{
+        slider.style.left -=  + "px";
+    }
 }
