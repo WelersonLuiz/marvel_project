1d8a426b949d6818340aec5afa2ecfb0be829bf5
Welerson <luizpawlak7@gmail.com>
2019-05-13 16:38:04 -0300

    Correção de bug no slider da página index.html

--- js/index.js
+++ js/index.js
@@ -15,6 +15,9 @@ var myTimer = setInterval(function rollSlide(){
     nextSlide();
     }, 5000);
 
+//Inicia a posição da variável para manipulação.
+container_position.style.left = "0px";
+
 //Função passa pro próximo slide
 function nextSlide(){
     //Verifica se o index de imagem está no range correto e resseta se estourar
@@ -26,34 +29,31 @@ function nextSlide(){
     container_position.style.left = (-1000*slideIndex) + "px";
 
     //Resseta o timer de 5 segundos
-    clearInterval(myTimer);
-    myTimer = setInterval(function rollSlide(){    
+    clearInterval(this.myTimer);
+    this.myTimer = setInterval(function rollSlide(){    
         nextSlide();
-        }, 7000);
+        }, 5000);
     
     //Incrementa o slide;
     slideIndex++;
 }
 
 function previousSlide(){
+    //Decrementa o slide;
+    slideIndex--;
+
     if (slideIndex == 0){
         //Seta a posição do container com relação ao index do slide
         container_position.style.left = (-1000*(slides.length-1)) + "px";
         slideIndex = slides.length;
-        
-        //Incrementa o slide;
-        slideIndex--;
     }else{
         //Seta a posição do container com relação ao index do slide
-        container_position.style.left = (-1000*(slideIndex-1)) + "px";
-        
-        //Incrementa o slide;
-        slideIndex--;
+        container_position.style.left = (-1000*(slideIndex-1)) + "px";  
     }
     
     //Resseta o timer de 5 segundos
-    clearInterval(myTimer);
-    myTimer = setInterval(function rollSlide(){    
+    clearInterval(this.myTimer);
+    this.myTimer = setInterval(function rollSlide(){    
         nextSlide();
-        }, 7000);
+        }, 5000);
 }
